b5675361342b47a24e6eddc293cb2b8d56a3ba0c
Derek Guenther <derek.guenther@coinbase.com>
2025-04-22 15:29:45 -0400

    fix: update ENR with new cgc

--- packages/beacon-node/test/unit/network/metadata.test.ts
+++ packages/beacon-node/test/unit/network/metadata.test.ts
@@ -1,23 +1,38 @@
 import {ssz} from "@lodestar/types";
 import {toHex} from "@lodestar/utils";
-import {describe, expect, it} from "vitest";
-import {getENRForkID} from "../../../src/network/metadata.js";
+import {describe, expect, it, vi} from "vitest";
+import {ENRKey, MetadataController, getENRForkID} from "../../../src/network/metadata.js";
+import {NetworkConfig} from "../../../src/network/networkConfig.js";
+import {serializeCgc} from "../../../src/util/metadata.js";
 import {config} from "../../utils/config.js";
+import {getValidPeerId} from "../../utils/peer.js";
 
-describe("network / metadata / getENRForkID", () => {
-  // At 0, next fork is altair
-  const currentEpoch = 0;
-  const enrForkID = getENRForkID(config, currentEpoch);
+describe("network / metadata", () => {
+  describe("getENRForkID", () => {
+    // At 0, next fork is altair
+    const currentEpoch = 0;
+    const enrForkID = getENRForkID(config, currentEpoch);
 
-  it("enrForkID.nextForkVersion", () => {
-    expect(toHex(enrForkID.nextForkVersion)).toBe(toHex(config.ALTAIR_FORK_VERSION));
+    it("enrForkID.nextForkVersion", () => {
+      expect(toHex(enrForkID.nextForkVersion)).toBe(toHex(config.ALTAIR_FORK_VERSION));
+    });
+
+    it("enrForkID.nextForkEpoch", () => {
+      expect(enrForkID.nextForkEpoch).toBe(config.ALTAIR_FORK_EPOCH);
+    });
+
+    it("it's possible to serialize enr fork id", () => {
+      ssz.phase0.ENRForkID.hashTreeRoot(enrForkID);
+    });
   });
 
-  it("enrForkID.nextForkEpoch", () => {
-    expect(enrForkID.nextForkEpoch).toBe(config.ALTAIR_FORK_EPOCH);
-  });
-
-  it("it's possible to serialize enr fork id", () => {
-    ssz.phase0.ENRForkID.hashTreeRoot(enrForkID);
+  describe("MetadataController", () => {
+    it("should call onSetValue with the correct cgc", () => {
+      const onSetValue = vi.fn();
+      const networkConfig = new NetworkConfig(getValidPeerId(), config);
+      const metadata = new MetadataController({}, {onSetValue, networkConfig});
+      metadata.cgc = 128;
+      expect(onSetValue).toHaveBeenCalledWith(ENRKey.cgc, serializeCgc(128));
+    });
   });
 });
